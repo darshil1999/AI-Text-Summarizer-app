d62f638068042fd9132031a9e62def500891ec46
darshil1999 <darshilaslaliya@gmai.com>
2024-01-29 00:13:40 -0500

    endpoint ends

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-app.post('/summary', (req,res) => {
-  
+app.post('/summary', (req, res) => {
+    // get the text_to_summarize property from the request body
+    const text = req.body.text_to_summarize;
+
+    // call your summarizeText function, passing in the text from the request
+    summarizeText(text)
+        .then(response => {
+            res.send(response); // Send the summary text as a response
+        })
+        .catch(error => {
+            console.log(error.message);
+        });
 });
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
